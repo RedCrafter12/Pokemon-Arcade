1a4af945cf959c6d45e16d0e62909ddcc4e488f4
RedCrafter12 <pyahmadi2@gmail.com>
2025-10-05 20:34:45 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    tiles.placeOnTile(mySprite, mySprite.tilemapLocation())
+})
+let mySprite: Sprite = null
 tiles.setCurrentTilemap(tilemap`Level1`)
-let mySprite = sprites.create(img`
+mySprite = sprites.create(img`
     f f f f f f f f f f f f f f f f 
     f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
     f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
@@ -17,7 +21,8 @@ let mySprite = sprites.create(img`
     f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
     f f f f f f f f f f f f f f f f 
     `, SpriteKind.Player)
+let location = 0
 tiles.placeOnRandomTile(mySprite, sprites.dungeon.darkGroundCenter)
 tiles.setWallAt(tiles.getTileLocation(4, 2), true)
-controller.moveSprite(mySprite)
 scene.cameraFollowSprite(mySprite)
+controller.moveSprite(mySprite)
